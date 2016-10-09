1b0d83376c3efa10180b4dbcf2f074f561b2fa9b
po6xkernel <po6xsecpo@gmail.com>
2016-10-08 19:04:21 -0600

    Parseo de argumentos de linea de comandos

--- scheduler/RandomProcessGenerator.java
+++ scheduler/RandomProcessGenerator.java
@@ -1,5 +1,4 @@
-
-/* RandomProcessGenerator.java*/
+/* ProcessScheduler.java*/
 
 /**
  ** Hecho por: Diego Porras
@@ -7,7 +6,45 @@
  ** Seccion: AN
 **/ 
 
-public class RandomProcessGenerator{
+/*
+	Clase principal del sistema tiene 3 funciones pricipales:
+		1)Interpretar argumentos de linea de comandos.
+		2)Instanciacion y manejo de objeto cola/politica
+		3)Generacion aleatoria de procesos y su adicion a la cola/politica en ejecucion.
+*/
+public class ProcessScheduler{
+	private int pid=0;
+	//Tiempo default para cada tipo de proceso durante esta ejecucion.
+	private double aTime,lTime,cTime,ioTime;
+	//Ramngo de tiempo con el que  se crearan procesos
+	private double lowTime,upTime;
+	private int quantum=0;
+	private Policy politica;
+	private String nPolitica;
+	private final String FIRST_COME_FIRST_SERVED='fcfs';
+	private final String LAST_COME_FIRST_SERVED='lcfs';
+	private final String RR='rr';
 
-	
+	/*
+	*punto de entrada de la aplicacion.
+	*Lee los argumentos y crea las configuraciones iniciales para la ejecucion
+	*@param args[] Argumentos de linea de comandos. 
+	**/
+	public static void main(String args[]){
+		//Desempaquetado de parametros
+		String nPolitica=args[0].subString(1);
+		double lowTime=Double.parseDouble(args[1].split("-")[0]);
+		double upTime=Double.parseDouble(args[1].split("-")[1]);
+		double aTime=Double.parseDouble(args[2]);
+		double ioTime=Double.parseDouble(args[3]);
+		double cTime=Double.parseDouble(args[4]);
+		double lTime=Double.parseDouble(args[5]);
+		if(args.length == 7 ) double quantum=Double.parseDouble(args[6]);
+
+		//Parte operativa
+		ProcessScheduler pScheduler = new ProcessScheduler(aTime, lTime, cTime,
+		 ioTime, lowTime,upTime, nPolitica);
+	}
+
+
 }
